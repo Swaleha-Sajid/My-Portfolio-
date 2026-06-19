5b58d39c9e963f6abe076e277e2d9961215ffe6c
Swaleha-Sajid <visualnestdesign99@gmail.com>
2026-06-19 10:18:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+/* =========================
+   SCROLL ANIMATION
+========================= */
+
+const observer = new IntersectionObserver(
+
+(entries)=>{
+
+    entries.forEach((entry)=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},
+
+{
+    threshold:0.15
+}
+
+);
+
+document
+.querySelectorAll(
+".project-card,.service-card,.skill-pill,.contact-box"
+)
+.forEach((el)=>{
+
+    observer.observe(el);
+
+});
+
+
+/* =========================
+   NAVBAR EFFECT
+========================= */
+
+window.addEventListener("scroll",()=>{
+
+    const header =
+    document.querySelector(".header");
+
+    if(window.scrollY > 50){
+
+        header.style.background =
+        "rgba(15,23,42,.95)";
+
+        header.style.boxShadow =
+        "0 5px 20px rgba(0,0,0,.2)";
+
+    }
+
+    else{
+
+        header.style.background =
+        "rgba(15,23,42,.75)";
+
+        header.style.boxShadow =
+        "none";
+
+    }
+
+});
+
+
+/* =========================
+   SMOOTH BUTTON HOVER
+========================= */
+
+document
+.querySelectorAll(".btn")
+.forEach((btn)=>{
+
+    btn.addEventListener("mouseenter",()=>{
+
+        btn.style.transform =
+        "translateY(-4px)";
+
+    });
+
+    btn.addEventListener("mouseleave",()=>{
+
+        btn.style.transform =
+        "translateY(0px)";
+
+    });
+
+});
